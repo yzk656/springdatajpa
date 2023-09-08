b054200c5b3f64622fbb5eb58e8f56486cc66288
杨振坤 <2793942765@qq.com>
2023-09-08 17:44:53 +0800

    通过年龄进行排序查询

--- src/main/java/com/example/springdatajpa/Repository/PersonRepository.java
+++ src/main/java/com/example/springdatajpa/Repository/PersonRepository.java
@@ -1,10 +1,13 @@
 package com.example.springdatajpa.Repository;
 
 import com.example.springdatajpa.entity.Person;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
@@ -13,5 +16,8 @@ public interface PersonRepository extends JpaRepository<Person,Long> {
 
     @Query(value = "select * from person where name = :name",nativeQuery = true)
     Person findByName(@Param("name") String name);
+
+
+    List<Person> findByAgeGreaterThanEqual(Integer age, Pageable pageable);
 
 }
--- src/main/java/com/example/springdatajpa/controller/PersonController.java
+++ src/main/java/com/example/springdatajpa/controller/PersonController.java
@@ -4,7 +4,13 @@ package com.example.springdatajpa.controller;
 import com.example.springdatajpa.Repository.PersonRepository;
 import com.example.springdatajpa.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -28,4 +34,19 @@ public class PersonController {
         Person personRepositoryByName = personRepository.findByName(person.getName());
         System.out.println(personRepositoryByName.getId());
     }
+
+    /**
+     * 通过年龄进行排序查询
+     * @param age
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/getByAge")
+    public List<Person> getByAge(Integer age, int pageNo, int pageSize){
+        Pageable pageable=PageRequest.of(pageNo - 1, pageSize, Sort.by(Sort.Direction.ASC, "age"));
+        List<Person> page = personRepository.findByAgeGreaterThanEqual(age, pageable);
+        return page;
+    }
+
 }
